58fcad452e5dea75dc79872239b753c7bf8509b1
Abdallah Bratos <bratosab@gmail.com>
2019-06-23 21:15:07 +0200

    home page et menu principal

--- pwa-showcase-app/src/app/app.module.ts
+++ pwa-showcase-app/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { CrashDetectorModule } from './crash-detector/crash-detector.module';
 import { CustomMaterialModule } from './custom-material/custom-material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VideoCaptureModule } from './video-capture/video-capture.module';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, WelcomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
