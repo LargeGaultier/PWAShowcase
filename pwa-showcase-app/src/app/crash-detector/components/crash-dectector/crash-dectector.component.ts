078f1eb2412337ea96bedcae6d5429fe4ef540f1
Abdallah Bratos <bratosab@gmail.com>
2019-06-23 21:14:50 +0200

    branchement au module crash detector et algo de detection

--- pwa-showcase-app/src/app/crash-detector/components/crash-dectector/crash-dectector.component.ts
+++ pwa-showcase-app/src/app/crash-detector/components/crash-dectector/crash-dectector.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatBottomSheet } from '@angular/material';
+import { AutoIncidentSheetComponent } from '../auto-incident-sheet/auto-incident-sheet.component';
 
 @Component({
   selector: 'app-crash-dectector',
@@ -6,10 +8,83 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crash-dectector.component.scss']
 })
 export class CrashDectectorComponent implements OnInit {
+  lastX;
+  lastY;
+  lastZ;
+  moveCounter = 0;
+  motion;
+  isAutoSheetOpened = false;
 
-  constructor() { }
+  constructor(private bottomSheet: MatBottomSheet) {}
 
   ngOnInit() {
+    this.detectMotion();
   }
 
+  openAutoIncidentSheet() {
+    if (!this.isAutoSheetOpened) {
+      this.bottomSheet
+        .open(AutoIncidentSheetComponent)
+        .afterDismissed()
+        .subscribe(() => {
+          this.isAutoSheetOpened = false;
+        });
+      this.isAutoSheetOpened = true;
+    }
+  }
+
+  private detectMotion() {
+    window.addEventListener(
+      'devicemotion',
+      motiondata => {
+        this.handleMotion(motiondata);
+        this.motion = motiondata;
+      },
+      true
+    );
+  }
+
+  private handleMotion(e) {
+    let acc = e.acceleration;
+    if (!acc.hasOwnProperty('x')) {
+      acc = e.accelerationIncludingGravity;
+    }
+
+    if (!acc.x) {
+      return;
+    }
+
+    if (Math.abs(acc.x) >= 1 && Math.abs(acc.y) >= 1 && Math.abs(acc.z) >= 1) {
+      if (!this.lastX) {
+        this.lastX = acc.x;
+        this.lastY = acc.y;
+        this.lastZ = acc.z;
+        return;
+      }
+
+      const deltaX = Math.abs(acc.x - this.lastX);
+      const deltaY = Math.abs(acc.y - this.lastY);
+      const deltaZ = Math.abs(acc.z - this.lastZ);
+
+      if (deltaX + deltaY + deltaZ > 50) {
+        this.moveCounter++;
+      } else {
+        this.moveCounter = Math.max(0, --this.moveCounter);
+      }
+
+      if (this.moveCounter > 1) {
+        this.crashDetected();
+
+        this.moveCounter = 0;
+      }
+
+      this.lastX = acc.x;
+      this.lastY = acc.y;
+      this.lastZ = acc.z;
+    }
+  }
+
+  private crashDetected() {
+    this.openAutoIncidentSheet();
+  }
 }
--- pwa-showcase-app/src/app/crash-detector/crash-detector.module.ts
+++ pwa-showcase-app/src/app/crash-detector/crash-detector.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { CrashDetectorRoutingModule } from './crash-detector-routing.module';
+import { NgModule } from '@angular/core';
+import { CustomMaterialModule } from '../custom-material/custom-material.module';
+import { AutoIncidentSheetComponent } from './components/auto-incident-sheet/auto-incident-sheet.component';
 import { CrashDectectorComponent } from './components/crash-dectector/crash-dectector.component';
+import { CrashDetectorRoutingModule } from './crash-detector-routing.module';
 
 @NgModule({
-  declarations: [CrashDectectorComponent],
-  imports: [CommonModule, CrashDetectorRoutingModule]
+  declarations: [CrashDectectorComponent, AutoIncidentSheetComponent],
+  entryComponents: [AutoIncidentSheetComponent],
+  imports: [CommonModule, CrashDetectorRoutingModule, CustomMaterialModule]
 })
 export class CrashDetectorModule {}
